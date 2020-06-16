0fac215cb323b9ac042094e6ba2bb919efba8f97
Duy <nhuvanduy97@gmail.com>
2020-06-16 13:20:31 +0700

    update

---
+++ Assets/Scripts/slide28/AutoShowMoney.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class AutoShowMoney : MonoBehaviour
+{
+    public GameObject Money, CanvasFeedback;
+
+    public bool isClick = false;
+    void Start(){
+        StartCoroutine(Show(Money,0.5f));
+        StartCoroutine(Hidden(Money,2.0f));
+    }
+    IEnumerator Show(GameObject obj, float seconds) {
+        yield return new WaitForSeconds(seconds);
+        if (obj != null) {
+            bool isActive = obj.activeSelf;
+            MoneyModel.scoreValue += 120;
+            obj.SetActive(true);
+		}
+	}
+
+    IEnumerator Hidden(GameObject obj, float seconds) {
+        yield return new WaitForSeconds(seconds);
+        isClick = true;
+        obj.SetActive(false);
+	}
+
+    public void closePopupFeedback() {
+        if (isClick) {
+            CanvasFeedback.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/slide28/NextPopup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextPopup : MonoBehaviour
+{
+     public GameObject CanvasPopup, CanvasFeedback;
+     
+     public void NextPopupFeedback() {
+         CanvasPopup.SetActive(false);
+         if (CanvasFeedback != null) {
+             CanvasFeedback.SetActive(true);
+         }
+     }
+}
