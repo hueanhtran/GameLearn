e97b041be0fd3aed360cb9cbdb830df110358975
hueanhtran <hueanhtr@gmail.com>
2020-06-18 16:27:22 +0700

    update

--- Assets/Scripts/Slide 32/MainController30.cs
+++ Assets/Scripts/Slide 32/MainController30.cs
@@ -5,9 +5,41 @@ using UnityEngine.SceneManagement;
 
 public class MainController30 : MonoBehaviour
 {
-    public void LoadScene(int index)
+    public GameObject slide18, slide20, slide21;
+    public void next_l1()
     {
-        SceneManager.LoadScene(index);
+        slide18.SetActive(false);
+        if (slide20 != null)
+        {
+            bool isActive3 = slide18.activeSelf;
+            slide20.SetActive(true);
+        }
     }
-        
+    public void back_l1()
+    {
+        slide20.SetActive(false);
+        if (slide18 != null)
+        {
+            bool isActive3 = slide20.activeSelf;
+            slide18.SetActive(true);
+        }
+    }
+    public void next_l2()
+    {
+        slide20.SetActive(false);
+        if (slide21 != null)
+        {
+            bool isActive3 = slide21.activeSelf;
+            slide21.SetActive(true);
+        }
+    }
+    public void back_l2()
+    {
+        slide21.SetActive(false);
+        if (slide20 != null)
+        {
+            bool isActive3 = slide20.activeSelf;
+            slide20.SetActive(true);
+        }
+    }
 }
