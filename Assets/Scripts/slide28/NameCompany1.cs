669c7ea29079892dfbd5bda8f2b0e5320138ef22
Duy <nhuvanduy97@gmail.com>
2020-06-22 15:35:40 +0700

    update

---
+++ Assets/Scripts/slide28/NameCompany1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NameCompany1 : MonoBehaviour
+{
+    public static string tip1 = "";
+    Text str;
+    void Start() {
+        str = GetComponent<Text>();
+    }
+    void Update() {
+        str.text =  tip1;
+    }
+}
