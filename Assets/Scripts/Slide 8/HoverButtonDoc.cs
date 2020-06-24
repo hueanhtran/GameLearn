3a84537dd138e6cfc7d913188901d3d53f1883b6
hueanhtran <hueanhtr@gmail.com>
2020-06-24 16:29:02 +0700

    update

---
+++ Assets/Scripts/Slide 8/HoverButtonDoc.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class HoverButtonDoc : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject IconToGiayButtonHover;
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        IconToGiayButtonHover.SetActive(true);
+    }
+
+    // Detect when Cursor leaves the GameObject
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        IconToGiayButtonHover.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Slide 8/HoverButtonT.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class HoverButtonT : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject iconchuTButtonHover;
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        iconchuTButtonHover.SetActive(true);
+    }
+
+    // Detect when Cursor leaves the GameObject
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        iconchuTButtonHover.SetActive(false);
+    }
+}
