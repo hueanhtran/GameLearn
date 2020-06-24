db4dc06426696ff8925cb2df9f6822f548505266
Duy <nhuvanduy97@gmail.com>
2020-06-24 10:56:51 +0700

    commit

--- Assets/Scripts/CheckEmptyName.cs
+++ Assets/Scripts/CheckEmptyName.cs
@@ -6,12 +6,15 @@ using UnityEngine.SceneManagement;
 
 public class CheckEmptyName : MonoBehaviour
 {
-     public void CheckIsEmptyName(InputField input){
+    public void CheckIsEmptyName(InputField input){
         StartCoroutine(Check(1.0f, input));
 	}
     IEnumerator Check(float seconds,InputField input) {
         yield return new WaitForSeconds(seconds);
         if (input.text != "") {
+            NameCompany1.tip1 = input.text + " rất hiểu khách và có dịch vụ chăm sóc rất tuyệt vời.";
+            NameCompany2.tip2 = "Sản phẩm của " + input.text + " không quá tệ. Nhưng tôi nghĩ họ vẫn nên cải thiện dịch vụ chăm sóc khách hàng.";
+            NameCompany3.tip3 = "Sản phẩm của " + input.text + " không quá tệ. Nhưng tôi nghĩ họ vẫn nên cải thiện dịch vụ chăm sóc khách hàng.";
             SceneManager.LoadScene(5);
 		}
 	}
---
+++ Assets/Scripts/slide28/NameCompany3.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class NameCompany3 : MonoBehaviour
+{
+    public static string tip3 = "";
+    Text str;
+    void Start() {
+        str = GetComponent<Text>();
+    }
+    void Update() {
+        str.text =  tip3;
+    }
+}
